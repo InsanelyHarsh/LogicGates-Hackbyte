8ee29d510958e01e6c8efe8cca11307d6e463980
janmesh kumar <janmesh799@gmail.com>
2023-04-08 04:30:40 +0530

    Created User and Post schema

--- web/server/Routes/User.js
+++ web/server/Routes/User.js
@@ -3,4 +3,5 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', (req, res) => { res.json({ success: true, message: "REached user" }) })
+
 module.exports = router
--- web/server/models/User.js
+++ web/server/models/User.js
@@ -13,7 +13,51 @@ const UserSchema = new schema({
     username: {
         type: String,
         required: true,
+    },
+    password: {
+        type: String, required: true
+    },
+    authToken: {
+        type: String,
+        required: true
+    },
+    experience: [{
+        company: {
+            type: String
+        },
+        role: {
+            type: String
+        },
+        employer: {
+            type: String
+        },
+        Month: {
+            type: Number
+        },
+        year: {
+            type: Number
+        }
+    }],
+    bio: {
+        type: String
+    },
+    preferences: [
+        String
+    ],
+    tags: [String],
+    posts: [
+        {
+            type: schema.types.ObjectId,
+            ref: "post"
+        }
+    ],
+    userType: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Float32Array
     }
 })
 
-module.exports = Teacher = mongoose.model('user', UserSchema);
+module.exports = User = mongoose.model('user', UserSchema);
